38d6e9b0ed899eb9a64a87d0c130ae2c3920eb07
la-selva <louis.laselva@gmail.com>
2015-09-29 15:43:27 +0200

    first commit

---
+++ javascript.js
@@ -0,0 +1,16 @@
+(function($){
+	$(document).ready(function(){
+		$('ul.dropdown-menu [data-toggle=dropdown]').on('click', function(event) {
+			event.preventDefault(); 
+			event.stopPropagation(); 
+			$(this).parent().siblings().removeClass('open');
+			$(this).parent().toggleClass('open');
+		});
+		$('.dropdown').on('hover', function(event) { 
+			$(this).parent().siblings().removeClass('open');
+			$(this).parent().toggleClass('open');
+			event.preventDefault(); 
+			event.stopPropagation();
+		});
+	});
+})(jQuery);
